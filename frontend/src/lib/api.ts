10753be6d745fcdb2b8559232fc047404194a0d2
Jacob Rader <jacob.rader@baruchmail.cuny.edu>
2026-06-10 10:11:37 -0400

    configuration debugging

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
-export const API_BASE_URL = (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000').replace(/\/+$/, '');
+export const API_BASE_URL = (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5003').replace(/\/+$/, '');
 
 export const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
